d78a4fd6389fbd23a70e0cb3bbbf6e04f84c7aac
srushhh86 <srushhh86@gmail.com>
2026-06-29 12:21:58 +0530

    second commit

--- tests/smoke/LoginVerify.spec.js
+++ tests/smoke/LoginVerify.spec.js
@@ -1,12 +1,12 @@
-import {expect, test} from "@playwright/test"
-import { LoginPage } from "../../POM/LoginPage"
+// import {expect, test} from "@playwright/test"
+// import { LoginPage } from "../../POM/LoginPage"
 
-import logindata from "../../TestData/commondata.json"
+// import logindata from "../../TestData/commondata.json"
 
-test("verify Login",async ({page}) => {
+// test("verify Login",async ({page}) => {
     
-    let login=new LoginPage(page)
-    await login.loginToPage(logindata.url,logindata.username,logindata.password)
-    await expect(page).toHaveURL("https://testyantra.espocloud.eu/")
+//     let login=new LoginPage(page)
+//     await login.loginToPage(logindata.url,logindata.username,logindata.password)
+//     await expect(page).toHaveURL("https://testyantra.espocloud.eu/")
 
-})
+// })
